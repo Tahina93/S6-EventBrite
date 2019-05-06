1c42cbc1f1ad1ebad8302fb71d3511a634849f4f
Tahina93 <anne.tahina.metz@gmail.com>
2019-05-06 20:22:58 +0200

    starting second mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,11 @@ class UserMailer < ApplicationMailer
 
   	mail(to: @user.email, subject: 'Bienvenue à toi, nouvelle créature !')
   end
+
+  def new_participant_email(user)
+  	@admin = 
+  	@participant =
+
+  	mail(to: @admin, subject: 'Un nouveau participant pour ton event !!')
+  end
 end
