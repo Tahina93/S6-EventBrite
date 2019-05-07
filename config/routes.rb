544b34bd314ef65496df9bb0210b4df6818803e5
Tahina93 <anne.tahina.metz@gmail.com>
2019-05-07 12:12:19 +0200

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'events/index'
   get 'static_page/index'
   get 'static_page/secret'
   devise_for :users
-  root 'static_page#index'
+  root 'events#index'
+
+  resource :users
+  resource :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
