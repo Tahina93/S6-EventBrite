4cad64ff3ccde621a09a2da2357e8ee51ff3bb22
SedTheCursed <sed.robert@gmail.com>
2019-05-07 13:00:37 +0200

    Work on views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,11 @@
 class EventsController < ApplicationController
   def index
+    @page_title = "Bienvenue"
+    @events = Event.all
+  end
+
+  def new
+    @page_title = "Ajouter un event"
+    @event = Event.new
   end
 end
