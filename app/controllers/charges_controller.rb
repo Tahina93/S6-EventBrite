f8b44a8118c504cb5518fa735e6a9e266fa8f335
Tahina93 <anne.tahina.metz@gmail.com>
2019-05-08 16:52:59 +0200

    payment

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,7 +5,7 @@ class ChargesController < ApplicationController
 
   def create
     # Amount in cents
-    @amount = @event.price
+    @amount = params[:amount]
 
     customer = Stripe::Customer.create({
       email: params[:stripeEmail],
@@ -18,6 +18,7 @@ class ChargesController < ApplicationController
       description: 'Rails Stripe customer',
       currency: 'eur',
     })
+    puts "************* #{params[:amount]}*********"
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
